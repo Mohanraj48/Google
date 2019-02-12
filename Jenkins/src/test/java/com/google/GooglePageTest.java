071fc4c2b9e586e7b0212da1c987fa32181a1c45
Mohanraj48 <mohanbca48@gmail.com>
2019-02-12 14:03:04 +0530

    Jenkins code pushed

---
+++ Jenkins/src/test/java/com/google/GooglePageTest.java
@@ -0,0 +1,31 @@
+package com.google;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class GooglePageTest 
+{
+
+	@Test
+	public void setup() throws Exception
+	{
+		
+		System.setProperty("webdriver.chrome.driver", "E:/Mohan/jarfiles/WebDrivers/Chrome/chromedriver_win32/chromedriver.exe");
+		WebDriver d=new ChromeDriver();
+		d.manage().window().maximize();
+		d.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		d.get("https://www.google.co.in");
+		Assert.assertTrue(d.getTitle().contains("Google"));
+		d.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		Thread.sleep(3000);
+		d.quit();
+		
+		
+	}
+	
+	
+}
